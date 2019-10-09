0ae26bff99b0b200fef43d9519ce54e64f3e80e4
Antonio Velazquez <anvelazq@microsoft.com>
2019-10-09 14:08:41 -0700

    Update test

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -10,7 +10,7 @@ namespace Microsoft.ML.Samples
 
         internal static void RunAll()
         {
-            int samples = 0;
+            /*int samples = 0;
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
             {
                 var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
@@ -23,7 +23,9 @@ namespace Microsoft.ML.Samples
                 }
             }
 
-            Console.WriteLine("Number of samples that ran without any exception: " + samples);
+            Console.WriteLine("Number of samples that ran without any exception: " + samples);*/
+            // ConsoleApp10ML.ConsoleApp.ModelBuilder.CreateModel();
+            // ConsoleApp10ML.ConsoleApp.ModelConsumer.Run();
         }
     }
 }
